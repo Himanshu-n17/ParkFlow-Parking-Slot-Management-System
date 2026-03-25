c59221ca4998c2f02266af98d6e9b2914873673a
Himanshu-n17 <nayak.himanshu2004@gmail.com>
2026-03-25 08:38:47 +0530

    Updated Authentication Services & Signup Page  for OTP Verification

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -9,3 +9,13 @@ export const registerUser = async (data) => {
   const response = await API.post("/auth/register", data);
   return response.data;
 };
+
+export const sendOtp = async (data) => {
+  const res = await API.post("/auth/send-otp", data);
+  return res.data;
+};
+
+export const verifyOtp = async (data) => {
+  const res = await API.post("/auth/verify-otp", data);
+  return res.data;
+};
